5331ffcef7941f006f2f855753dd630e08843490
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2021-04-07 11:01:17 -0400

    Delete pyin.bat

--- pyin.bat
+++
@@ -1,6 +0,0 @@
-pyinstaller cli.py ^
- --name=keyboard_draw ^
- --paths ../../resource_lib/src ^
- --add-data keyboard_draw_hello.txt;. ^
- --add-data hello_family.txt;. ^
- --add-data ../../resource_lib/images;./images
